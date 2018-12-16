371d637e968bb969b854bee781d4229b7391e1e1
ldolberg <lautaro.dolberg@gmail.com>
2018-12-17 00:39:47 +0100

    Day 12

--- day 11/main.clj
+++ day 11/main.clj
@@ -1,22 +1,10 @@
 (defn pl [cell]
     (let [pw (* (+ (* (+ 10 (first cell)) (second cell)) 8772) (+ 10 (first cell))) ]
-        (- (int (/ (mod pw 1000) 100)) 5)
-    )
-)
-(def grid (for [x (range 1 301) y (range 1 300)] 
-    {:cell [x y] :pw (pl [x y])}
-))
-
-; (apply max-key :pw grid)
-
+        (- (int (/ (mod pw 1000) 100)) 5)))
 (defn pgrid[n] (for [x (range 1 (- 301 n))
       y (range 1 (- 301 n))]
      (let [ subgrid (for [i (range 0 n) j (range 0 n)]
             [(+ x i) (+ y j)]
-        ) ] 
-        {:cell [x y] :pw (reduce + (map pl subgrid))})
-))
-
-(pmap  
-(fn[n] (println n (apply max-key :pw (pgrid n))))
-(range 1 300))
+        )] 
+        {:cell [x y] :pw (reduce + (map pl subgrid))})))
+(pmap  (fn[n] (println n (apply max-key :pw (pgrid n)))) (range 1 300))
